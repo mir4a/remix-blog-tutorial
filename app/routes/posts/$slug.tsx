fe4e80a3753f955aed3a58d8b50fea46ebe7b5c5
Miroslav Martynov <marty.mircha@gmail.com>
2022-11-07 12:46:15 +0100

    get post data

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,19 +1,19 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json({ slug: params.slug });
+  const post = await getPost(params.slug);
+  return json({ post });
 };
 
 export default function PostSlug() {
-  const { slug } = useLoaderData();
+  const { post } = useLoaderData();
 
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {slug}
-      </h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
     </main>
   );
 }
